8e78f162ca837af61cebda531ee04f2e1b1fd844
Jason <sysbender@gmail.com>
2024-02-01 13:36:57 -0500

    integrated monoca-editor

--- notes.md
+++ notes.md
@@ -1,2 +1,11 @@
 - setup the project:
   - https://github.com/gitname/react-gh-pages
+
+```
+"Service Worker registration failed: "
+
+Error: Failed to register a ServiceWorker for scope ('https://2-11-2-sandpack.codesandbox.io/') with script ('https://2-11-2-sandpack.codesandbox.io/csb-sw.js'): The script has an unsupported MIME type ('text/html').
+```
+
+last working version:
+npm i @codesandbox/sandpack-react@1.14.2
--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import { Sandpack } from "@codesandbox/sandpack-react";
+import React from "react";
+import Editor from "@monaco-editor/react";
 
-function App() {
+import {
+  useActiveCode,
+  SandpackStack,
+  FileTabs,
+  useSandpack,
+} from "@codesandbox/sandpack-react";
+
+import {
+  SandpackProvider,
+  SandpackLayout,
+  SandpackPreview,
+} from "@codesandbox/sandpack-react";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MySandpack
+      template="react"
+      theme="dark"
+      options={{
+        showConsole: true,
+        showLineNumbers: true,
+        showTabs: true,
+        showNavigator: true,
+
+        // readOnly: true,
+      }}
+      files={{
+        "/App.js": `export default function App() {
+        return <h1>Hello Sandpack 111</h1>
+      }`,
+        "App.css": `h1{color:red;}`,
+      }}
+    />
   );
 }
 
-export default App;
+export function App1() {
+  return (
+    <Sandpack
+      template="react"
+      theme="dark"
+      options={{
+        showConsole: true,
+        showLineNumbers: true,
+        showTabs: true,
+        showNavigator: true,
+
+        // readOnly: true,
+      }}
+      files={{
+        "/App.js": `export default function App() {
+        return <h1>Hello Sandpack 111</h1>
+      }`,
+      }}
+    />
+  );
+}
+export function App2() {
+  return (
+    <Editor
+      height="90vh"
+      defaultLanguage="javascript"
+      defaultValue="// some comment"
+    />
+  );
+}
+
+function MonacoEditor() {
+  const { code, updateCode } = useActiveCode();
+  const { sandpack } = useSandpack();
+
+  return (
+    <SandpackStack style={{ height: "100vh", margin: 0 }}>
+      <FileTabs />
+      <div style={{ flex: 1, paddingTop: 8, background: "#1e1e1e" }}>
+        <Editor
+          width="100%"
+          height="100%"
+          language="javascript"
+          theme="vs-dark"
+          key={sandpack.activeFile}
+          defaultValue={code}
+          onChange={(value) => updateCode(value || "")}
+        />
+      </div>
+    </SandpackStack>
+  );
+}
+export function MySandpack() {
+  return (
+    <SandpackProvider template="react" theme="dark">
+      <SandpackLayout>
+        <MonacoEditor /> // Your Monaco Editor Component
+        <SandpackPreview style={{ height: "100vh" }} />
+      </SandpackLayout>
+    </SandpackProvider>
+  );
+}
